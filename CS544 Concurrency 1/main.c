c8f9b4e18c6a5910cf5ee859431922f6d54b8c5f
David Greiner <mail@david-greiner.me>
2017-10-09 15:00:44 -0700

    Implement FIFO for buffer

--- CS544 Concurrency 1/main.c
+++ CS544 Concurrency 1/main.c
@@ -6,12 +6,55 @@
 //  Copyright © 2017 David Greiner. All rights reserved.
 //
 
+#include <stdlib.h>
 #include <stdio.h>
 
-struct item {
+typedef struct Item {
     int value;
     int waiting_period;
-};
+}Item;
+
+typedef struct FIFO {
+    int front, rear, size;
+    unsigned capacity;
+    int* array;
+}FIFO;
+
+FIFO* create_FIFO(unsigned capacity) {
+    FIFO* fifo = (FIFO*) malloc(sizeof(FIFO));
+    fifo->capacity = capacity;
+    fifo->front = fifo->size = 0;
+    fifo->rear = capacity - 1;
+    fifo->array = (int*) malloc(fifo->capacity * sizeof(int));
+    return fifo;
+}
+
+int is_full(FIFO* fifo) {
+    return (fifo->size == fifo->capacity);
+}
+
+int is_empty(FIFO* fifo) {
+    return (fifo->size == 0);
+}
+
+void enqueue(FIFO* fifo, int item)
+{
+    if(is_full(fifo))
+        return;
+    fifo->rear = (fifo->rear + 1) % fifo->capacity;
+    fifo->array[fifo->rear] = item;
+    fifo->size = fifo->size + 1;
+}
+
+int dequeue(FIFO* fifo)
+{
+    if(is_empty(fifo))
+        return -1;
+    int item = fifo->array[fifo->front];
+    fifo->front = (fifo->front + 1) % fifo->capacity;
+    fifo->size = fifo->size - 1;
+    return item;
+}
 
 int rdrand16_step (uint32_t *rand)
 {
